a56e316098c698c34ae87d00888d02a1c43ed1b0
KOKOROG <47898925+kokorog@users.noreply.github.com>
2022-03-11 01:39:05 -0800

    Update main.lua

--- interact-sound/client/main.lua
+++ interact-sound/client/main.lua
@@ -1,11 +1,11 @@
 local standardVolumeOutput = 0.3;
 local hasPlayerLoaded = false
 
-RegisterNetEvent('IFCore:Client:OnPlayerLoaded', function()
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     hasPlayerLoaded = true
 end)
 
-RegisterNetEvent('IFCore:Client:OnPlayerUnload', function()
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
     hasPlayerLoaded = false
 end)
 
